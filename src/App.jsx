8e529f322a9253817ed7c41606d321e984316842
Sammith1 <hsammith@gmail.com>
2022-08-04 21:04:56 +0530

    added background image

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Dashbord } from "./Componets/Dashbord";
+
+function App() {
+  return (
+    <div>
+      <Dashbord />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componets/Dashbord.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+export const Dashbord = () => {
+  return (
+    <>
+      <div className="GeeksForGeeks">
+        <div className="first_left_chart">
+          <span>10,356</span>
+        </div>
+      </div>
+    </>
+  );
+};
