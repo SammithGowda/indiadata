c327ff2d5b9a0adf8aee7f738be9bbb93ac16dd6
Sammith1 <hsammith@gmail.com>
2022-08-06 14:16:15 +0530

    avg time adopt div design

--- src/Componets/Dashbord.jsx
+++ src/Componets/Dashbord.jsx
@@ -1,5 +1,5 @@
 import "../App.css";
-import { Line } from "react-chartjs-2";
+import { Line, Bar } from "react-chartjs-2";
 // eslint-disable-next-line
 import Chart from "chart.js/auto";
 export const Dashbord = () => {
@@ -10,9 +10,10 @@ export const Dashbord = () => {
       legend: {
         display: false,
       },
-      title: {
-        display: false,
-        text: "Chart.js Line Chart",
+    },
+    layout: {
+      padding: {
+        left: 0,
       },
     },
     scales: {
@@ -58,21 +59,47 @@ export const Dashbord = () => {
       },
     ],
   };
+
   return (
     <>
       <div className="bacground_img_div">
         <div className="first_left_chart">
-          <span>10,356</span>
+          <span>10,857</span>
           <Line
             style={{
               //   border: "2px solid red",
-              width: "100%",
+              width: "90%",
+              height: "91%",
               margin: "auto",
-              height: "90%",
             }}
             options={options}
             data={data}
           />
+        </div>
+        {/*middle div with graph*/}
+        <div className="middle_graph_div">
+          <span>45 Days</span>
+          <p>Avg Time to Adopt by Age Bucket</p>
+          <div className="first_samll_graph">
+            {/*first 30days */}
+            <div>
+              <span className="span_style_class">0-1 Puppy</span>
+              <div className="firts_div"></div>{" "}
+              <span className="span_style_class">30days</span>
+            </div>
+            {/*second 80days */}
+            <div>
+              <span className="span_style_class">2-6 Adult</span>
+              <div className="second_div"></div>{" "}
+              <span className="span_style_class">80 days</span>
+            </div>
+            {/*third 103days */}
+            <div>
+              <span className="span_style_class">7+ Senior</span>
+              <div className="third_div"></div>{" "}
+              <span className="span_style_class">103 days</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
